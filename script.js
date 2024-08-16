774729c88556eecd2cf3942cf026a1d55cd7468b
Malak-Azzam <azzammalak353@gmail.com>
2024-08-16 17:48:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutMeBtn = document.querySelector('.btn');
+    aboutMeBtn.addEventListener('click', function() {
+        window.location.href = 'about.html';
+    });
+});
